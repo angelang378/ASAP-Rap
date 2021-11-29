e04a43739ba4a8712f258792cb0bf214f899581f
Rachel Lu <lurachel18@gmail.com>
2021-11-29 14:48:38 -0500

    Add musixmatch to get lyrics

--- music_data.py
+++ music_data.py
@@ -3,14 +3,17 @@ from dotenv import load_dotenv
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 import pandas as pd
+import requests
 
 load_dotenv()
-cid = os.getenv('SPOTIFY_CLIENT_ID')
-secret = os.getenv('SPOTIFY_CLIENT_SECRET')
+spotify_cid = os.getenv('SPOTIFY_CLIENT_ID')
+spotify_secret = os.getenv('SPOTIFY_CLIENT_SECRET')
+musixmatch_auth = os.getenv('MUSIXMATCH_AUTH_CODE')
 
-client_credentials_manager = SpotifyClientCredentials(client_id=cid, client_secret=secret)
+client_credentials_manager = SpotifyClientCredentials(client_id=spotify_cid, client_secret=spotify_secret)
 sp = spotipy.Spotify(client_credentials_manager = client_credentials_manager)
 
+# Use Spotify API to get all the songs from a given playlist
 def get_playlist(creator, playlist_id):
     playlist_features_list = ["artist","album","track_name","track_id","danceability","energy","key","loudness","mode","speechiness","instrumentalness","liveness","valence","tempo","duration_ms","time_signature"]
     playlist_df = pd.DataFrame(columns = playlist_features_list)
@@ -36,7 +39,26 @@ def get_playlist(creator, playlist_id):
 
     return playlist_df
 
+# Get the Beatles and top charting songs
 beatles = get_playlist("andream4273","1Gf0v4DneJjq3adPSiNVe6")
 beatles["track_name"] = beatles["track_name"].str.replace(r'-[^-]+$', "", regex=True)
-
 charting = get_playlist("Spotify", "37i9dQZF1DX5Ejj0EkURtP")
+
+# Get the lyrics for a given song
+def get_lyrics(track, artist):
+    track = track.replace(" ", "%20")
+    artist = artist.replace(" ", "%20")
+    link = "https://api.musixmatch.com/ws/1.1/matcher.lyrics.get?format=json&callback=callback&q_track="+track+"&q_artist="+artist+"&apikey="+musixmatch_auth
+    response = requests.get(link)
+    try:
+        return response.json()['message']['body']['lyrics']['lyrics_body'].split("...")[0]
+    except:
+        return None
+
+# Get the corresponding lyrics to the Beatles and top charting songs
+beatles['lyrics'] = beatles.apply(lambda track: get_lyrics(track['track_name'], track['artist']), axis=1)
+charting['lyrics'] = charting.apply(lambda track: get_lyrics(track['track_name'], track['artist']), axis=1)
+
+# Save playlists and info to CSVs
+beatles.to_csv("data/beatles.csv")
+charting.to_csv("data/charting.csv")
